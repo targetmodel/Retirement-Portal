2e3b30a33e48c267bd45ee0643e410623cf9d669
Anil <targetmodel@gmail.com>
2014-06-18 18:04:18 +0530

    added files

---
+++ js/app.js
@@ -0,0 +1,11 @@
++function(){
+
+//  $("body").on("click",".helout", function() {
+//    $(".panel-collapse").collapse('hide');
+//    $(this).removeClass('helout').addClass('helin');
+//    $(this).html('Expand');
+//  });
+//
+//  $(".panel-collapse").collapse({toggle:false});
+
+}();
